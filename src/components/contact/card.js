37f9cce08633447c11895d3fd092c5c943f15cf1
Mats Nord <nord.mats@gmail.com>
2018-02-23 14:24:49 +0100

    Style and structure for list vs single view

--- src/components/contact/card.js
+++ src/components/contact/card.js
@@ -6,20 +6,34 @@ import Styles from './card.css'
 
 const Card = ({person, fullInfo}) => {
   
-  console.info('FullInfo..., ', fullInfo)
+  console.info('FullInfo..., ', fullInfo);
+  const imgSize = fullInfo ? 'large' : 'medium';
   // TODO: Create utility/component for date formatting
-  const dob = person.dob.substring(0,10);
-  const registered = person.registered.substring(0,10);
-
+  // {`btn-group pull-right ${this.props.showBulkActions ? 'shown' : 'hidden'}`}
   return (
     <div className="card">
-      <div className="header">
+      <div className={`header ${fullInfo ? '' : ' lower'}`}>
         <h1><Name className={Styles.name} name={person.name.first} /> <Name className={Styles.name} name={person.name.last} /> </h1>
       </div>
       <div className="row">
-      <div className="col-2 picture">
-        <img src={person.picture.large} alt="" />
-      </div> 
+        <div className="col-2 picture">
+          <img src={person.picture[imgSize]  } alt="" />
+        </div>
+        { 
+          fullInfo 
+          ? ( <FullInfo person={person} /> )  
+          : ( <ListInfo person={person} /> )
+        }
+      </div>
+    </div>
+  )
+};
+
+const FullInfo = ({person}) => {
+  const dob = formatDate(person.dob);
+  const registered = formatDate(person.registered);
+  return (
+    <div>
       <div className="col-9 info info-block">
         <InfoItem infoText="Title" info={person.name.title} />
         <InfoItem infoText="Phone" info={person.phone} />
@@ -27,10 +41,23 @@ const Card = ({person, fullInfo}) => {
         <EmailItem infoText="Email" email={person.email} />
         <InfoItem infoText="Day of birth" info={dob} />
         <InfoItem infoText="Registered" info={registered} />
-      </div>
+        <p>A really long text here. A really long text here. A really long text here. A really long text here. A really long text here. A really long text here. A really long text here. A really long text here. A really long text here. </p> 
       </div>
     </div>
-  )
-};
+)};
+
+const ListInfo = ({person}) => {
+  const dob = formatDate(person.dob);
+  const registered = formatDate(person.registered);
+  return (
+    <div className="col-9 info info-block">
+    <InfoItem infoText="Title" info={person.name.title} />
+    <InfoItem infoText="Phone" info={person.phone} />
+    <InfoItem infoText="Cell phone" info={person.cell} />
+    <EmailItem infoText="Email" email={person.email} />
+  </div>
+)};
+
+const formatDate = (str) => str.substring(0,10);
 
 export default Card;
